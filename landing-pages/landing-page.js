9d723270480650e841486accb8b21c14cddeceb7
Rosa <kosolrosa@gmail.com>
2023-10-29 17:46:41 +1100

    Finalised css for landing page 2

--- landing-pages/landing-page.js
+++ landing-pages/landing-page.js
@@ -3,6 +3,18 @@ $('.hamburger').click (function(){
     $(this).toggleClass('is-active');
   });
 
+$('.carousel').carousel({
+  interval: false
+})
+
+$('.carousel-control-prev').click(function() {
+  $('.carousel').carousel('prev');
+});
+
+$('.carousel-control-next').click(function() {
+  $('.carousel').carousel('next');
+});
+
 // When the user scrolls down 20px from the top of the document, shrink the navbar padding slightly
 window.onscroll = function() {scrollFunction()};
 
@@ -13,3 +25,4 @@ function scrollFunction() {
     document.getElementById("navbar").style.padding = "15px 15px";
   }
 }
+
